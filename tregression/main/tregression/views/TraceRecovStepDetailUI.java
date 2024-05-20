9000b826e8c556257f0e287af5e4a6eb6d727284
HongshuW <hongshu.wang.uni@gmail.com>
2024-05-20 21:49:34 +0800

    Add context analysis button

---
+++ tregression/main/tregression/views/TraceRecovStepDetailUI.java
@@ -0,0 +1,90 @@
+package tregression.views;
+
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+
+import microbat.model.trace.Trace;
+import microbat.model.trace.TraceNode;
+import microbat.model.value.VarValue;
+import microbat.tracerecov.CandidateVarRetriever;
+
+/**
+ * A subclass of StepDetailUI with context scope analysis.
+ * 
+ * @author hongshuwang
+ */
+public class TraceRecovStepDetailUI extends StepDetailUI {
+
+	public TraceRecovStepDetailUI(TregressionTraceView view, TraceNode node, boolean isOnBefore) {
+		super(view, node, isOnBefore);
+	}
+
+	@Override
+	protected void createSlicingGroup(Composite panel) {
+		Group slicingGroup = new Group(panel, SWT.NONE);
+		GridData data = new GridData(SWT.FILL, SWT.TOP, true, false);
+		data.minimumHeight = 35;
+		slicingGroup.setLayoutData(data);
+
+		GridLayout gl = new GridLayout(3, true);
+		slicingGroup.setLayout(gl);
+
+		dataButton = new Button(slicingGroup, SWT.RADIO);
+		dataButton.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false));
+		dataButton.setText("data ");
+
+		controlButton = new Button(slicingGroup, SWT.RADIO);
+		controlButton.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false));
+		controlButton.setText("control ");
+
+		Button submitButton = new Button(slicingGroup, SWT.NONE);
+		submitButton.setText("Go");
+		submitButton.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, false));
+		FeedbackSubmitListener fListener = new FeedbackSubmitListener();
+		submitButton.addMouseListener(fListener);
+
+		/* Added by hongshuwang */
+		Button contextAnalysisButton = new Button(slicingGroup, SWT.NONE);
+		contextAnalysisButton.setText("Analyse Context");
+		contextAnalysisButton.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, false));
+		ContextAnalysisListener cListener = new ContextAnalysisListener();
+		contextAnalysisButton.addMouseListener(cListener);
+	}
+
+	class ContextAnalysisListener implements MouseListener {
+		
+		public void mouseUp(MouseEvent e) {}
+		
+		public void mouseDoubleClick(MouseEvent e) {}
+		
+		public void mouseDown(MouseEvent e) {
+			Trace trace = traceView.getTrace();
+			
+			Object[] objList = readVariableTreeViewer.getCheckedElements();
+			if (objList.length != 0) {
+				Object obj = objList[0];
+				if(obj instanceof VarValue) {
+					VarValue readVar = (VarValue)obj;
+					
+					/* get candidate variables */
+					List<String> candidateVariables = CandidateVarRetriever.getCandidateVariables(currentNode.getInvokingMethod());
+					readVar.setCandidateVariables(candidateVariables);
+					
+					/* context scope analysis */
+					trace.findRecoveredDataDependency(currentNode, readVar);
+					readVariableTreeViewer.refresh();
+					
+					/* simulate execution */
+				}
+			}
+		}
+	}
+}
