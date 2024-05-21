953c95d38097224e2ca746d39ddf6a11e0bfda06
HongshuW <hongshu.wang.uni@gmail.com>
2024-05-21 17:47:10 +0800

    Update data recovery algorithm

--- tregression/main/tregression/views/TraceRecovStepDetailUI.java
+++ tregression/main/tregression/views/TraceRecovStepDetailUI.java
@@ -1,6 +1,8 @@
 package tregression.views;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
@@ -14,7 +16,8 @@ import org.eclipse.swt.widgets.Group;
 import microbat.model.trace.Trace;
 import microbat.model.trace.TraceNode;
 import microbat.model.value.VarValue;
-import microbat.tracerecov.CandidateVarRetriever;
+import microbat.tracerecov.candidatevar.CandidateVarRetriever;
+import microbat.tracerecov.executionsimulator.ExecutionSimulator;
 
 /**
  * A subclass of StepDetailUI with context scope analysis.
@@ -60,29 +63,49 @@ public class TraceRecovStepDetailUI extends StepDetailUI {
 	}
 
 	class ContextAnalysisListener implements MouseListener {
-		
-		public void mouseUp(MouseEvent e) {}
-		
-		public void mouseDoubleClick(MouseEvent e) {}
-		
+
+		public void mouseUp(MouseEvent e) {
+		}
+
+		public void mouseDoubleClick(MouseEvent e) {
+		}
+
 		public void mouseDown(MouseEvent e) {
 			Trace trace = traceView.getTrace();
-			
+
 			Object[] objList = readVariableTreeViewer.getCheckedElements();
 			if (objList.length != 0) {
 				Object obj = objList[0];
-				if(obj instanceof VarValue) {
-					VarValue readVar = (VarValue)obj;
-					
-					/* get candidate variables */
-					List<String> candidateVariables = CandidateVarRetriever.getCandidateVariables(currentNode.getInvokingMethod());
+				if (obj instanceof VarValue) {
+					VarValue readVar = (VarValue) obj;
+
+					/* Candidate Variables Identification */
+					/* Identify candidate variables through Java bytecode analysis (static) */
+					List<String> candidateVariables = CandidateVarRetriever
+							.getCandidateVariables(currentNode.getInvokingMethod());
 					readVar.setCandidateVariables(candidateVariables);
-					
-					/* context scope analysis */
-					trace.findRecoveredDataDependency(currentNode, readVar);
+
+					/* Context Scope Analysis */
+					/* 1. Relevant Step Identification: Identify relevant steps by variable ID
+					 * 2. Variable Mapping: Link candidate variables to variables on trace
+					 * 3. Repeat step (1) and (2) to find the relevant steps for each candidate variable */
+					Map<String, List<TraceNode>> relevantSteps = trace.findRecoveredDataDependency(currentNode,
+							readVar);
+
+					/* Execution Simulation */
+					/* Simulate execution by calling LLM model */
+					try {
+						ExecutionSimulator executionSimulator = new ExecutionSimulator(relevantSteps, currentNode, readVar);
+						List<String> response = executionSimulator.sendRequests();
+						System.out.println();
+					} catch (IOException ioException) {
+						ioException.printStackTrace();
+					}
+
+					/* Variable Mapping */
+					/* Add critical variables identified by the LLM to the trace */
+
 					readVariableTreeViewer.refresh();
-					
-					/* simulate execution */
 				}
 			}
 		}
