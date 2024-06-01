1a8d3056d74c04369265c3ab4c45015508db8efe
peri103 <yunruipei@163.com>
2024-06-01 11:37:38 +0800

    Resolve merge conflict in tregression/plugin.xml

--- tregression/main/tregression/empiricalstudy/config/Defects4jProjectConfig.java
+++ tregression/main/tregression/empiricalstudy/config/Defects4jProjectConfig.java
@@ -47,7 +47,7 @@ public class Defects4jProjectConfig extends ProjectConfig{
 		else if (projectName.equals("Gson")) {
 			config = new MavenProjectConfig("gson"+File.separator+"src"+File.separator+"test"+File.separator+"java", "gson"+File.separator+"src"+File.separator+"main"+File.separator+"java", "target"+File.separator+"test-classes", "target"+File.separator+"classes", "target", projectName, regressionID);
 		}
-		else if (projectName.equals("JacksonCore") || projectName.equals("JacksonDatabind") || projectName.equals("JacksonXml") || projectName.equals("Jsoup")) {
+		else if (projectName.equals("JacksonCore") || projectName.equals("JacksonDatabind") || projectName.equals("JacksonXml") || projectName.equals("Jsoup") || projectName.equals("Zip4j")) {
 			config = new MavenProjectConfig("src"+File.separator+"test"+File.separator+"java", "src"+File.separator+"main"+File.separator+"java", "target"+File.separator+"test-classes", "target"+File.separator+"classes", "target", projectName, regressionID);
 		}
 		else if (projectName.equals("JxPath")) {
@@ -99,6 +99,10 @@ public class Defects4jProjectConfig extends ProjectConfig{
 			else{
 				config = new Defects4jProjectConfig("src"+File.separator+"test"+File.separator+"java", "src"+File.separator+"main"+File.separator+"java", "build"+File.separator+"tests", "build"+File.separator+"classes", "build", projectName, regressionID);
 			}
+		}
+		else if (projectName.equals("addthis_stream-lib")) {
+			config = new MavenProjectConfig("src"+File.separator+"test"+File.separator+"java", "src"+File.separator+"main"+File.separator+"java", "target"+File.separator+"test-classes", "target"+File.separator+"classes", "target", projectName, regressionID);				
+			
 		}
 		
 		return config;
