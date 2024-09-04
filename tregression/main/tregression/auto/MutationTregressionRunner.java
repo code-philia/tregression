5a66f68e2dcb5459050165e467002df3021e94a9
HaHaKwy <1625827540@qq.com>
2024-09-04 15:04:48 +0800

    run on mutation bugs

--- tregression/main/tregression/auto/MutationTregressionRunner.java
+++ tregression/main/tregression/auto/MutationTregressionRunner.java
@@ -1,7 +1,9 @@
 package tregression.auto;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,6 +14,7 @@ import java.util.List;
 import microbat.model.trace.Trace;
 import tregression.auto.result.RunResult;
 import tregression.empiricalstudy.EmpiricalTrial;
+import tregression.empiricalstudy.config.MutationConfig;
 import tregression.empiricalstudy.config.ProjectConfig;
 import tregression.empiricalstudy.config.TraceRecovMutationConfig;
 
@@ -22,15 +25,13 @@ public class MutationTregressionRunner extends ProjectsRunner {
 	private String projectsBasePath;
 	private String mutationFileBasePath;
 	private String workingBasePath;
-	private String resultPath;
 
 	public MutationTregressionRunner(String projectsBasePath, String mutationFileBasePath, String workingBasePath,
 			String resultPath) {
-		super(workingBasePath, resultPath, 5);
+		super(mutationFileBasePath, resultPath, 5);
 		this.projectsBasePath = projectsBasePath;
 		this.mutationFileBasePath = mutationFileBasePath;
 		this.workingBasePath = workingBasePath;
-		this.resultPath = resultPath;
 	}
 
 	@Override
@@ -45,7 +46,7 @@ public class MutationTregressionRunner extends ProjectsRunner {
 		result.projectName = projectName;
 		result.bugID = Integer.valueOf(bugID_str);
 
-		final ProjectConfig config = TraceRecovMutationConfig.getConfig(projectName, bugID_str);
+		final ProjectConfig config = MutationConfig.getConfig(projectName, bugID_str);
 		if (config == null) {
 			result.errorMessage = ProjectsRunner.genMsg("Cannot generate project config");
 			return result;
@@ -67,6 +68,8 @@ public class MutationTregressionRunner extends ProjectsRunner {
 		List<EmpiricalTrial> trials = this.generateTrials(bugFolder, fixFolder, config);
 		if (trials == null || trials.isEmpty()) {
 			result.errorMessage = ProjectsRunner.genMsg("No trials generated");
+			File dir = new File(Paths.get(this.workingBasePath,projectName).toString());
+			deleteDirectory(dir);
 			return result;
 		}
 
@@ -76,6 +79,8 @@ public class MutationTregressionRunner extends ProjectsRunner {
 			Trace trace = trial.getBuggyTrace();
 			if (trace == null) {
 				result.errorMessage = "[Trials Generation]: " + trial.getExceptionExplanation();
+				File dir = new File(Paths.get(this.workingBasePath,projectName).toString());
+				deleteDirectory(dir);
 				return result;
 			}
 			result.traceLen = Long.valueOf(trace.size());
@@ -86,8 +91,10 @@ public class MutationTregressionRunner extends ProjectsRunner {
 			result.simulationTime = trial.getSimulationTime();
 		}
 
-		// TODO 3. delete the previous bug-fix folder
-
+		// 3. delete the previous bug-fix folder
+		File dir = new File(Paths.get(this.workingBasePath,projectName).toString());
+		deleteDirectory(dir);
+		
 		return result;
 	}
 
@@ -110,52 +117,72 @@ public class MutationTregressionRunner extends ProjectsRunner {
 				Files.copy(source, target2, StandardCopyOption.REPLACE_EXISTING);
 
 			} catch (Exception e) {
-				System.err.println("--ERROR-- In copy project!");
 				e.printStackTrace();
 			}
 		});
 
 		// 3. substitute files with mutated ones
 		Path mutationFileFolderPath = Paths.get(this.mutationFileBasePath, projectName, bugID_str);
+
+		File mutatedJavaFile = getSingleJavaFile(mutationFileFolderPath.toString()); // MuClass.class
+
+		String simpleClassName = mutatedJavaFile.getName(); // MuClass.java
+		simpleClassName = simpleClassName.substring(0, simpleClassName.indexOf(".")); // MuClass
+		String fullClassName = getClassFullName(mutatedJavaFile.getAbsolutePath(), simpleClassName);// org.jfree.chart.text.MuClass
+		if(fullClassName == null) {
+			System.out.println("__ERROR__ get full class name failed!");
+		}
 		
-		File mutatedClassFile = getSingleClassFile(mutationFileFolderPath.toString()); // MuClass.class
-		
-		String simpleClassName = mutatedClassFile.getName(); // MuClass.class
-		simpleClassName = simpleClassName.substring(0,simpleClassName.indexOf(".")); // MuClass
-		String fullClassName = getClassFullName(Paths.get(mutatedClassFile.getAbsolutePath()), simpleClassName); // org.jfree.chart.text.MuClass
-		
-		String tmp = fullClassName.replaceAll(".", File.separator); // org\jfree\chart\text\MuClass
-		Path srcFilePathInProject = Paths.get(bugFolder.toString(),config.srcSourceFolder,tmp+".java"); // D:\MutationWorkSpace\Chart\2\bug\source\org\jfree\chart\text\MuClass.java
-		Path classFilePathInProject = Paths.get(bugFolder.toString(),config.bytecodeSourceFolder,tmp+".class"); // D:\MutationWorkSpace\Chart\2\bug\build\org\jfree\chart\text\TextLine.class
-	
-		Files.copy(Paths.get(mutationFileFolderPath.toString(),simpleClassName+".java"), srcFilePathInProject, StandardCopyOption.REPLACE_EXISTING);
-		Files.copy(Paths.get(mutationFileFolderPath.toString(),simpleClassName+".class"), classFilePathInProject, StandardCopyOption.REPLACE_EXISTING);
-		Files.copy(Paths.get(mutationFileFolderPath.toString(),"failing_tests"), Paths.get(bugFolder.toString(),"failing_tests"), StandardCopyOption.REPLACE_EXISTING);
-		
+		String tmp = fullClassName.replace(".", File.separator); // org\jfree\chart\text\MuClass
+		Path srcFilePathInProject = Paths.get(bugFolder.toString(), config.srcSourceFolder, tmp + ".java"); // D:\MutationWorkSpace\Chart\2\bug\source\org\jfree\chart\text\MuClass.java
+		Path classFilePathInProject = Paths.get(bugFolder.toString(), config.bytecodeSourceFolder, tmp + ".class"); // D:\MutationWorkSpace\Chart\2\bug\build\org\jfree\chart\text\TextLine.class
+
+		Files.copy(Paths.get(mutationFileFolderPath.toString(), simpleClassName + ".java"), srcFilePathInProject,
+				StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(Paths.get(mutationFileFolderPath.toString(), simpleClassName + ".class"), classFilePathInProject,
+				StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(Paths.get(mutationFileFolderPath.toString(), "failing_tests"),
+				Paths.get(bugFolder.toString(), "failing_tests"), StandardCopyOption.REPLACE_EXISTING);
+
 		System.out.println("finish creating bug-fix folder");
 	}
 
-	public static File getSingleClassFile(String directoryPath) {
+	public static File getSingleJavaFile(String directoryPath) {
 		File directory = new File(directoryPath);
 		File[] allFiles = directory.listFiles();
 		for (File file : allFiles) {
-			if(file.getName().endsWith(".class")) {
+			if (file.getName().endsWith(".java")) {
 				return file;
 			}
 		}
 		return null;
 	}
-	
-    public static String getClassFullName(Path classFilePath, String simpleClassName) {
-    	// TODO get full className through .class file and simpleClassName
-        try (FileInputStream fis = new FileInputStream(classFilePath.toFile())) {
-            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-            Class<?> clazz = classLoader.loadClass(simpleClassName);
-            return clazz.getName();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
+	public static String getClassFullName(String javaFilePath, String simpleClassName) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(javaFilePath))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.startsWith("package ")) {
+					String packageName = line.substring(8, line.indexOf(';')).trim();
+					return packageName + "." + simpleClassName;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+    public static void deleteDirectory(File directory) {
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    deleteDirectory(file);
+                }
+            }
+        }
+        directory.delete();
+    }
 }
