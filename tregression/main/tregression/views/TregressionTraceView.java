459706fffa37c7686fdf8e8cfffd846576780cf4
HongshuW <hongshu.wang.uni@gmail.com>
2024-12-23 17:58:46 +0800

    fix bug in On-Click Request

--- tregression/main/tregression/views/TregressionTraceView.java
+++ tregression/main/tregression/views/TregressionTraceView.java
@@ -1,5 +1,11 @@
 package tregression.views;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import org.eclipse.jface.action.Action;
@@ -12,15 +18,61 @@ import org.eclipse.ui.IActionBars;
 
 import microbat.Activator;
 import microbat.model.trace.TraceNode;
+import microbat.ondemandtrace.views.ExpandTraceOptions;
 import microbat.views.ImageUI;
 import microbat.views.TraceView;
+import tregression.empiricalstudy.TestCase;
+import tregression.handler.PathConfiguration;
 import tregression.model.PairList;
+import tregression.preference.TregressionPreference;
 import tregression.separatesnapshots.DiffMatcher;
 
 public abstract class TregressionTraceView extends TraceView {
 	protected PairList pairList;
 	protected DiffMatcher diffMatcher;
-	
+
+	@Override
+	protected MenuManager createExpandTraceMenu(MenuManager parentMenuMgr) {
+		String projectPath = Activator.getDefault().getPreferenceStore().getString(TregressionPreference.PROJECT_NAME);
+		String bugID = Activator.getDefault().getPreferenceStore().getString(TregressionPreference.BUG_ID);
+
+		String buggyPath = PathConfiguration.getBuggyPath(projectPath, bugID);
+
+		try {
+			List<TestCase> testCases = retrieveFailingTestCase(buggyPath);
+			TestCase defaultTC = testCases.get(0); // TODO: other test cases? consider other naming convension
+			String className = defaultTC.testClass;
+			String testCase = defaultTC.testMethod;
+			ExpandTraceOptions options = new ExpandTraceOptions(listViewer.getSelection(), className, testCase);
+			return options.getOptions();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private List<TestCase> retrieveFailingTestCase(String buggyVersionPath) throws IOException {
+		String failingFile = buggyVersionPath + File.separator + "failing_tests";
+		File file = new File(failingFile);
+
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+
+		List<TestCase> list = new ArrayList<>();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (line.startsWith("---")) {
+				String testClass = line.substring(line.indexOf(" ") + 1, line.indexOf("::"));
+				String testMethod = line.substring(line.indexOf("::") + 2, line.length());
+				System.currentTimeMillis();
+				TestCase tc = new TestCase(testClass, testMethod);
+				list.add(tc);
+			}
+		}
+		reader.close();
+
+		return list;
+	}
+
 	@Override
 	protected void appendMenuForTraceStep() {
 		menuMgr.setRemoveAllWhenShown(true);
@@ -35,7 +87,7 @@ public abstract class TregressionTraceView extends TraceView {
 				menuMgr.add(expandTraceOptions);
 			}
 		});
-		
+
 		listViewer.getTree().setMenu(menuMgr.createContextMenu(listViewer.getTree()));
 	}
 
